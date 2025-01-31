1208486f601283c371f02a17d8e8b68eb88f427f
Aswindevpk <aswindevpk014@gmail.com>
2025-01-31 23:31:55 +0530

    feat(async counter): async update on counter using asyncThunk, add React Thunk addifodd for sync condition

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,17 +1,22 @@
 import { useSelector, useDispatch } from "react-redux"
 import type { RootState, AppDispatch } from "../../app/store"
-import { increment, decrement, reset, incrementByAmount } from "../counter/counterSlice"
+import { increment, decrement, reset, incrementByAmount, incrementAsync, incrementIfOdd} from "../counter/counterSlice"
 
-const Counter = ()=>{
-    const count = useSelector((state:RootState)=>state.counter.value)
+const Counter = () => {
+    const count = useSelector((state: RootState) => state.counter.value)
+    const status = useSelector((state: RootState) => state.counter.status)
     const dispatch = useDispatch<AppDispatch>()
-    return(
+    return (
         <div>
             <h1>Counter:{count}</h1>
-            <button onClick={()=>dispatch(increment())}>+</button>
-            <button onClick={()=>dispatch(decrement())}>-</button>
-            <button onClick={()=>dispatch(reset())}>Reset</button>
-            <button onClick={()=>dispatch(incrementByAmount(2))}>+2</button>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(reset())}>Reset</button>
+            <button onClick={() => dispatch(incrementByAmount(2))}>+2</button>
+            <button onClick={() => dispatch(incrementIfOdd(5))}>AddIfOdd</button>
+            <button onClick={() => dispatch(incrementAsync(10))} disabled={status === 'loading'}>
+                {status === "loading" ? "loading":"Increment Async (10)"}
+            </button>
         </div>
     )
 }
